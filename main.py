589c571e486aab1aed86a84c850b1e40f3144265
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-01-17 18:06:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("List generator")
+Start=int(input("Choose a starting number"))
+End=int(input("Choose a ending number."))
+increments=int(input("Choose a increment to add each time."))
+
+for i in range(Start,End,increments):
+  print(i)
+  
+  
+    
